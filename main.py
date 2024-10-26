e20539309d9524e5af41f24691a6492ee35f1291
Scorpitary <josh2tarr@gmail.com>
2024-10-25 22:24:12 -0700

    starting asteroids

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+import constants
+
+def main():
+    print("Starting asteroids!")
+    print("Screen width: 1280")
+    print("Screen height: 720")
+
+if __name__ == "__main__":
+    main()
