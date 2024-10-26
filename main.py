1642ab163f1030986a1ca352032a377217efb735
Scorpitary <josh2tarr@gmail.com>
2024-10-26 15:56:54 -0700

    working background and git ignore

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ import pygame
 import constants
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     print("Starting asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
