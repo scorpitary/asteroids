fc5902ec41c3eb61f551ef5d5d7d8fcd05755fb1
Scorpitary <josh2tarr@gmail.com>
2024-10-27 11:21:29 -0700

    added a triangle for the player

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import pygame
-import constants
+from constants import *
+from circleshape import *
+from player import *
+clock = pygame.time.Clock()
+dt = 0
+
+player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
