4df170c9650e9f14d8d0835bd9456e5084c07c53
Scorpitary <josh2tarr@gmail.com>
2024-10-27 11:58:11 -0700

    added player movement

--- main.py
+++ main.py
@@ -19,11 +19,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        dt = clock.tick(60) / 1000
+        
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
 
-        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
